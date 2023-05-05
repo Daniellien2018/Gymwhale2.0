bbad23e2bc74de5ee8c43eb119e804e210e5a84c
daniellien2018 <daniellien2018@gmail.com>
2023-05-05 16:26:33 -0700

    databaseissues

--- app.py
+++ app.py
@@ -3,44 +3,69 @@ from flask_dance.contrib.google import make_google_blueprint, google
 import logging
 import os
 from dotenv import load_dotenv
+from . import db
+
 
 load_dotenv()
-app = Flask(__name__)
-#Oauth google plus API
-client_id = os.getenv('GOOGLE_CLIENT_ID')
-client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
-app.secret_key = os.getenv('secret_key')
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-#accounts for google changing the requested OAuth scope
-os.environ['OAUTHLIB_INSECRE_TRANSPORT']='1'
-os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE']='1'
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    db.init_app(app)
 
-blueprint = make_google_blueprint(
-    client_id=client_id,
-    client_secret=client_secret,
-    reprompt_consent=True,
-    scope=["profile", "email"]
-)
-app.register_blueprint(blueprint, url_prefix="/login")
+    #Oauth google plus API
+    client_id = os.getenv('GOOGLE_CLIENT_ID')
+    client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+    app.secret_key = os.getenv('secret_key')
 
-@app.route("/")
-def index():
-    google_data = None
-    user_info_endpoint = '/oauth2/v2/userinfo'
-    if google.authorized:
-        google_data = google.get(user_info_endpoint).json()
+    #accounts for google changing the requested OAuth scope
+    os.environ['OAUTHLIB_INSECRE_TRANSPORT']='1'
+    os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE']='1'
 
-    return render_template('index.j2',
-                           google_data=google_data,
-                           fetch_url=google.base_url + user_info_endpoint)
+    blueprint = make_google_blueprint(
+        client_id=client_id,
+        client_secret=client_secret,
+        reprompt_consent=True,
+        scope=["profile", "email"]
+    )
+    app.register_blueprint(blueprint, url_prefix="/login")
 
-@app.route('/login')
-def login():
-    return redirect(url_for('google.login'))
+    @app.route("/")
+    def index():
+        google_data = None
+        user_info_endpoint = '/oauth2/v2/userinfo'
+        if google.authorized:
+            google_data = google.get(user_info_endpoint).json()
 
-@app.route("/signup/")
-def signup():
-    return "<p>This is the Signup page</p>"
+        return render_template('index.j2',
+                            google_data=google_data,
+                            fetch_url=google.base_url + user_info_endpoint)
 
-if __name__ == "__main__":
-    app.run()
+    @app.route('/login')
+    def login():
+        return redirect(url_for('google.login'))
+
+    @app.route("/signup/")
+    def signup():
+        return "<p>This is the Signup page</p>"
+
+    if __name__ == "__main__":
+        app.run()
+    
+    return app
